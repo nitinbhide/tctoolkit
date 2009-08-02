be9b5e24787046757a608e5f118f23f0a97a5a82
Nitin Bhide <31431663+nitinbhide@users.noreply.github.com>
2009-08-02 15:34:47 +0000

    setup files.

---
+++ src/setup.py
@@ -0,0 +1,24 @@
+'''
+setup.py
+Copyright (C) 2009 Nitin Bhide (nitinbhide@gmail.com)
+
+This module is part of Thinking Craftsman Toolkit (TC Toolkit).
+and is released under the New BSD License: http://www.opensource.org/licenses/bsd-license.php
+TC Toolkit is hosted at http://code.google.com/p/tctoolkit/
+--------------------------------------------------------------------------------------
+Setup file for installing Thinking Craftsman Toolkit
+'''
+
+from distutils.core import setup
+
+setup(name='TCToolkit', version ='0.5.0',
+      description='python module to analyze source code in various ways',
+      author='Nitin Bhide',
+      author_email='nitinbhide@gmail.com',
+      license = 'http://www.opensource.org/licenses/bsd-license.php',
+      url='http://code.google.com/p/tctoolkit',
+      requires = ['pygments'],
+      packages=['tctoolkit', 'tctoolkit.codedupdetect','tctoolkit.tctoolkitutil', 'tctoolkit.tokentagcloud'],
+      package_dir = {'': '.'},
+      package_data= {'tctoolkit':['README.htm']}
+     )
