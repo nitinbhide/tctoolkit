14e4124893209d4d665d15a5885a9a378091fd04
nitinbhide <nitinbhide@fc5449c8-75b5-11de-95bf-e7e72f2c99b1>
2009-09-03 14:43:37 +0000

    modification to output the duplicates to a text file (or stdout) and display it to treemap.

--- src/tctoolkit/codedupdetect/codedupdetect.py
+++ src/tctoolkit/codedupdetect/codedupdetect.py
@@ -14,36 +14,43 @@ from rabinkarp import RabinKarp
 from tokenizer import Tokenizer
 
 class CodeDupDetect:
-    def __init__(self,minmatch=100):
+    def __init__(self,filelist, minmatch=100):
         self.matchstore = matchstore.MatchStore(minmatch)
         self.minmatch = minmatch #minimum number of tokens to be matched.
+        self.filelist = filelist
+        self.foundcopies = False
 
-    def findcopies(self, filelist):
-        totalfiles = len(filelist)
+    def __findcopies(self):
+        totalfiles = len(self.filelist)
         i=0
-        for srcfile in filelist:
+        for srcfile in self.filelist:
             i=i+1
             print "Analyzing file %s (%d of %d)" %(srcfile,i,totalfiles)
             tknzr = Tokenizer(srcfile)
             rk = RabinKarp(self.minmatch,self.matchstore)
             rk.addAllTokens(tknzr)            
-            
+        self.foundcopies = True
+        
+    def findcopies(self):
+        if( self.foundcopies == False):
+            self.__findcopies()
         return(self.matchstore.iter_matches())
 
-    def printmatches(self,filelist):
-        exactmatches = self.findcopies(filelist)
+    def printmatches(self,output):
+        exactmatches = self.findcopies()
         #now sort the matches based on the matched line count (in reverse)
         exactmatches = sorted(exactmatches,reverse=True,key=lambda x:x.matchedlines)
         matchcount=0
         
         for matches in exactmatches:
             matchcount=matchcount+1
-            print "Match %d:"%matchcount
-            print "number of duplications :%d" % len(matches)
+            output.write("Match %d:\n"%matchcount)
+            output.write("number of duplications :%d\n" % len(matches))
             for match in matches:
-                print "file : %s start line: %d duplicate lines:%d" % (match.srcfile(),match.getStartLine(),match.getLineCount())
+                output.write("\tfile : %s start line: %d duplicate lines:%d\n"
+                             % (match.srcfile(),match.getStartLine(),match.getLineCount()))
                 
-            print "***"
+            output.write("***\n")
 
         return(exactmatches)            
 
