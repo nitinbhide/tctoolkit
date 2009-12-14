89e44397cbf77552eb14fc30852ab7927af08644
nitinbhide@fc5449c8-75b5-11de-95bf-e7e72f2c99b1 <nitinbhide@fc5449c8-75b5-11de-95bf-e7e72f2c99b1>
2009-12-14 14:19:55 +0000

    added the options parser for commandline options processing.

--- src/tctoolkit/depends.py
+++ src/tctoolkit/depends.py
@@ -4,21 +4,33 @@ Uses Pygments to quickly parse source files.
 '''
 
 import sys
+import string
+from optparse import OptionParser
 
 from tcdepends.dependency import Dependency
 
 def RunMain():
-    language='cpp'
-    if( language == 'cpp'):
-        dirname = "E:\\users\\nitinb\\sources\\tctools\\test\\apache_httpd"
-        pathlist = ["E:\\users\\nitinb\\sources\\tctools\\test\\apache_httpd\\include"]
-    if( language == 'java'):
-        dirname = "E:\\users\\nitinb\\sources\\tctools\\test\\tomcat\\java"
-        pathlist = ["E:\\users\\nitinb\\sources\\tctools\\test\\tomcat\\java"]
-        
-    #dirname = sys.argv[1]
-    dep = Dependency(language, pathlist)
-    dep.printDependency(dirname)
+    usage = "usage: %prog [options] <directory name>"
+    parser = OptionParser(usage)
+
+    parser.add_option("-l", "--lang", dest="lang", default='cpp',
+                      help="programming language to determine the dependencies (cpp, java or c#)")
+    parser.add_option("-I", "--includes", dest="includespath", default='.',
+                      help="list of include paths (seperated by ;)")
+    
+    (options, args) = parser.parse_args()
+
+    if( len(args) < 1):
+        print "Invalid number of arguments. Use depends.py --help to see the details."
+    else:        
+        dirname = args[0]
+        pathlist = options.includespath.split(';')
+        pathlist.append('.') #append current directory to search path
+        print "Language : %s" % (options.lang)
+        print "Dependency search path : %s" % options.includespath
+        print "Searching dependencies ..."
+        dep = Dependency(options.lang, pathlist)
+        dep.printDependency(dirname)
     
 if __name__ == "__main__":
     RunMain()
