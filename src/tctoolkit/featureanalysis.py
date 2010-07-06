5748a10ef6cc877d5e8f9d7e0d6f5ca3c759f468
nitinbhide@fc5449c8-75b5-11de-95bf-e7e72f2c99b1 <nitinbhide@fc5449c8-75b5-11de-95bf-e7e72f2c99b1>
2010-07-06 16:47:46 +0000

    added feature analysis based on 'Non Negative Matrix Factorization' from Programming Collective Intelligence by Toby Segaran.

---
+++ src/tctoolkit/featureanalysis.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+'''
+featureanalysis.py
+Do a feature analysis using NNMF (ref. Programming Collective Intelligence) of mulitple
+source files to find common features across multiple files.
+
+Copyright (C) 2009 Nitin Bhide (nitinbhide@gmail.com, nitinbhide@thinkingcraftsman.in)
+
+This module is part of Thinking Craftsman Toolkit (TC Toolkit) and is released under the
+New BSD License: http://www.opensource.org/licenses/bsd-license.php
+TC Toolkit is hosted at http://code.google.com/p/tctoolkit/
+'''
+
+import string
+import sys,fnmatch
+import math
+from optparse import OptionParser
+
+from tctoolkitutil.common import PreparePygmentsFileList
+from featureanalysis.featureanalysis import FeatureAnalysis
+
+def AnalyzeFeatures(dirname, pattern,outfilename):
+    filelist = PreparePygmentsFileList(dirname)
+    filelist = fnmatch.filter(filelist,pattern)
+    
+    feat = FeatureAnalysis()
+    feat.detectFeatures(filelist)
+    print "printing features"
+    with open(outfilename, "wb") as outfile:
+        feat.printFeatures(outfile)
+    
+def TestMain():
+    AnalyzeFeatures('./test/apache_httpd', '*.c', "features.txt")
+    #AnalyzeFeatures('./test/tomcat', '*.java', "features.txt")
+    #AnalyzeFeatures('./test/ccnet/core', '*.cs')
+    
+def RunMain():
+    usage = "usage: %prog [options] <directory name>"
+    parser = OptionParser(usage)
+
+    parser.add_option("-p", "--pattern", dest="pattern", default='*.c',
+                      help="do feature analysis of files matching the pattern")
+    parser.add_option("-o", "--outfile", dest="outfile", default=None,
+                      help="outfile name. Output to stdout if not specified")
+    
+    (options, args) = parser.parse_args()
+    
+    if( len(args) < 1):
+        print "Invalid number of arguments. Use featureanalysis.py --help to see the details."
+    else:        
+        dirname = args[0]
+            
+        tmplDict = dict()
+        tagcld = AnalyzeFeatures(dirname, options.pattern)
+        
+        tmplDict['KEYWORD_TAGSTR']= tagcld.getTagCloudHtml(filterFunc=KeywordFilter)
+        tmplDict['NAME_TAGSTR']= tagcld.getTagCloudHtml(filterFunc=NameFilter)
+        tmplDict['CLASSFUNCNAME_TAGSTR']= tagcld.getTagCloudHtml(filterFunc=ClassFuncNameFilter)
+        
+        OutputTagCloud(options.outfile, tmplDict)
+        
+if(__name__ == "__main__"):
+    #RunMain()
+    TestMain()
+    
+    
