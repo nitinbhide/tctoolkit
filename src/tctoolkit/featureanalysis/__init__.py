e1cdbbf0dda5c630d472d3b6e2c7a997cb6e18e1
Nitin Bhide <31431663+nitinbhide@users.noreply.github.com>
2010-07-06 16:47:46 +0000

    added feature analysis based on 'Non Negative Matrix Factorization' from Programming Collective Intelligence by Toby Segaran.

---
+++ src/tctoolkit/featureanalysis/__init__.py
@@ -0,0 +1,7 @@
+'''
+Copyright (C) 2009 Nitin Bhide (nitinbhide@gmail.com, nitinbhide@thinkingcraftsman.in)
+
+This module is part of Thinking Craftsman Toolkit (TC Toolkit) and is released under the
+New BSD License: http://www.opensource.org/licenses/bsd-license.php
+TC Toolkit is hosted at http://code.google.com/p/tctoolkit/
+'''
