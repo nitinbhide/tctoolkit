fed65cd9ec490e642de9f3acb8321db145e18526
nitinbhide@fc5449c8-75b5-11de-95bf-e7e72f2c99b1 <nitinbhide@fc5449c8-75b5-11de-95bf-e7e72f2c99b1>
2010-07-10 14:40:40 +0000

    fix for python 2.5

--- src/tctoolkit/featureanalysis.py
+++ src/tctoolkit/featureanalysis.py
@@ -10,6 +10,7 @@ This module is part of Thinking Craftsman Toolkit (TC Toolkit) and is released u
 New BSD License: http://www.opensource.org/licenses/bsd-license.php
 TC Toolkit is hosted at http://code.google.com/p/tctoolkit/
 '''
+from __future__ import with_statement
 
 import string
 import sys,fnmatch
