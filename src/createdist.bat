f9a5fa700df769f3134a73e61a1fdbdd12bd6e86
nitinbhide@fc5449c8-75b5-11de-95bf-e7e72f2c99b1 <nitinbhide@fc5449c8-75b5-11de-95bf-e7e72f2c99b1>
2010-07-12 04:27:30 +0000

    minor fixes.

--- src/createdist.bat
+++ src/createdist.bat
@@ -1,5 +1,6 @@
 @echo off
 REM Creating binrary and source distribution of SVNPlot
+del manifest
 echo "Creating Thinking Craftsman Toolkit source distribution in zip format"
 setup.py sdist --formats=zip
 echo "Creating Windows installer for Thinking Craftsman Toolkit "
