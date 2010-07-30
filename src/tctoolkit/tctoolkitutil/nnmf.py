b7417753cb1ffe9148ac57953e4ce57d4654568e
nitinbhide@fc5449c8-75b5-11de-95bf-e7e72f2c99b1 <nitinbhide@fc5449c8-75b5-11de-95bf-e7e72f2c99b1>
2010-07-30 03:13:28 +0000

    added 'weight' to words based on the log of word frequency. Seems to be giving better results in grouping similar files together.

--- src/tctoolkit/tctoolkitutil/nnmf.py
+++ src/tctoolkit/tctoolkitutil/nnmf.py
@@ -30,6 +30,8 @@ def factorize(v,pc=10,iter=50):
     ic=shape(v)[0]
     fc=shape(v)[1]
   
+    print 'pc=%f'%pc
+    print 'ic=%f'%ic
     # Initialize the weight and feature matrices with random values
     w=matrix([[random.random() for j in range(pc)] for i in range(ic)])
     h=matrix([[random.random() for i in range(fc)] for i in range(pc)])
@@ -40,7 +42,6 @@ def factorize(v,pc=10,iter=50):
         wh=w*h
         # Calculate the current difference
         cost=difcost(v,wh)
-        
         if i%10==0: print cost
         
         # Terminate if the matrix has been fully factorized
