a47a21f34ee56db03d25643182ca426b869bc703
nitinbhide <nitinbhide@localhost>
2013-10-15 18:23:27 +0530

    added to check to ignore words in stop words list.

--- src/tctoolkit/featureanalysis/featureanalysis.py
+++ src/tctoolkit/featureanalysis/featureanalysis.py
@@ -21,6 +21,38 @@ from tctoolkitutil import nnmf
 
 SPLIT_VAR_RE = re.compile("[A-Z_]+[a-z0-9]+")
 
+STOPWORDS_SET= set(["a", "about", "above", "above", "across", "after", "afterwards", "again", "against", "all",
+                    "almost", "alone", "along", "already", "also","although","always","am","among", "amongst",
+                    "amoungst", "amount",  "an", "and", "another", "any","anyhow","anyone","anything","anyway",
+                    "anywhere", "are", "around", "as",  "at", "back","be","became", "because","become","becomes",
+                    "becoming", "been", "before", "beforehand", "behind", "being", "below", "beside", "besides",
+                    "between", "beyond", "bill", "both", "bottom","but", "by", "call", "can", "cannot", "cant",
+                    "co", "con", "could", "couldnt", "cry", "de", "describe", "detail", "do", "done", "down",
+                    "due", "during", "each", "eg", "eight", "either", "eleven","else", "elsewhere", "empty",
+                    "enough", "etc", "even", "ever", "every", "everyone", "everything", "everywhere", "except",
+                    "few", "fifteen", "fify", "fill", "find", "fire", "first", "five", "for", "former", "formerly",
+                    "forty", "found", "four", "from", "front", "full", "further", "get", "give", "go", "had", "has",
+                    "hasnt", "have", "he", "hence", "her", "here", "hereafter", "hereby", "herein", "hereupon",
+                    "hers", "herself", "him", "himself", "his", "how", "however", "hundred", "ie", "if", "in", "inc",
+                    "indeed", "interest", "into", "is", "it", "its", "itself", "keep", "last", "latter", "latterly",
+                    "least", "less", "ltd", "made", "many", "may", "me", "meanwhile", "might", "mill", "mine", "more",
+                    "moreover", "most", "mostly", "move", "much", "must", "my", "myself", "name", "namely", "neither",
+                    "never", "nevertheless", "next", "nine", "no", "nobody", "none", "noone", "nor", "not", "nothing",
+                    "now", "nowhere", "of", "off", "often", "on", "once", "one", "only", "onto", "or", "other",
+                    "others", "otherwise", "our", "ours", "ourselves", "out", "over", "own","part", "per", "perhaps",
+                    "please", "put", "rather", "re", "same", "see", "seem", "seemed", "seeming", "seems", "serious",
+                    "several", "she", "should", "show", "side", "since", "sincere", "six", "sixty", "so", "some",
+                    "somehow", "someone", "something", "sometime", "sometimes", "somewhere", "still", "such",
+                    "system", "take", "ten", "than", "that", "the", "their", "them", "themselves", "then",
+                    "thence", "there", "thereafter", "thereby", "therefore", "therein", "thereupon", "these",
+                    "they", "thickv", "thin", "third", "this", "those", "though", "three", "through", "throughout",
+                    "thru", "thus", "to", "together", "too", "top", "toward", "towards", "twelve", "twenty", "two",
+                    "un", "under", "until", "up", "upon", "us", "very", "via", "was", "we", "well", "were", "what",
+                    "whatever", "when", "whence", "whenever", "where", "whereafter", "whereas", "whereby", "wherein",
+                    "whereupon", "wherever", "whether", "which", "while", "whither", "who", "whoever", "whole",
+                    "whom", "whose", "why", "will", "with", "within", "without", "would", "yet", "you", "your",
+                    "yours", "yourself", "yourselves", "the"]);
+
 def split_variable_name(variable):
     '''
     split variable names like GetSomething into 'get' and 'something'. Use
@@ -33,11 +65,11 @@ def split_variable_name(variable):
 def tokenize(fname):
     tokenzr = Tokenizer(fname)
     for ttype,tokenstr in tokenzr.get_tokens():
-        if( ttype in Token.Name):
-            if( ttype in Token.Name.Class or ttype in Token.Name.Function ):
+        if( ttype in Token.Name and (ttype in Token.Name.Class or ttype in Token.Name.Function) ):
                 #split the variable and function names in word.
                 for tk in split_variable_name(tokenstr):
-                    yield tk
+                    if tk not in STOPWORDS_SET:
+                        yield tk
 
 class FeatureAnalysis:
     def __init__(self):
