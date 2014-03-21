0384e9320e733846a6c953501509b7519e306d22
nitinbhide <none@none>
2014-03-21 17:45:58 +0530

    removed unused function. These were used in generating the tag cloud using HTML fonts.

--- src/tctoolkit/ttc.py
+++ src/tctoolkit/ttc.py
@@ -166,44 +166,7 @@ class HtmlSourceTagCloud(SourceCodeTagCloud):
 
     def __init__(self, dirname, pattern):
         super(HtmlSourceTagCloud, self).__init__(dirname, pattern)
-        self.maxFreqLog = 0.0
-        self.minFreqLog = 0.0
-        self.fontsizevariation = HtmlSourceTagCloud.MAXFONTSIZE-HtmlSourceTagCloud.MINFONTSIZE
-        assert(self.fontsizevariation > 0)
-        
-    def __getTagFontSize(self, freq):
-        #change the font size between MINFONTSIZE to MAXFONTSIZE relative to current font size
-        #now calculate the scaling factor for scaling the freq to fontsize.
-        scalingFactor = self.fontsizevariation/(self.maxFreqLog-self.minFreqLog)
-        fontsize = int(HtmlSourceTagCloud.MINFONTSIZE+((math.log(freq)-self.minFreqLog)*scalingFactor)+0.5)
-        #now round off to ensure that font size remains in MINFONTSIZE and MAXFONTSIZE
-        assert(fontsize >= HtmlSourceTagCloud.MINFONTSIZE and fontsize <= HtmlSourceTagCloud.MAXFONTSIZE)
-        return(fontsize)
-    
-    def getTagCloudHtml(self,numWords=100, filterFunc=None):
-        tagHtmlStr = ''
-
-        tagWordList = self.getTags(numWords, filterFunc)
                 
-        if( len(tagWordList) > 0):                        
-            minFreq = min(tagWordList,key=operator.itemgetter(1))[1]
-            self.minFreqLog = math.log(minFreq)            
-            maxFreq = max(tagWordList,key=operator.itemgetter(1))[1]
-            self.maxFreqLog = math.log(maxFreq)
-            difflog = self.maxFreqLog-self.minFreqLog
-            #if the minfreqlog and maxfreqlog are nearly same then makesure that difference is at least 0.001 to avoid
-            #division by zero errors later.
-            assert(difflog >= 0.0)
-            if( difflog < 0.001):
-                self.maxFreqLog = self.minFreqLog+0.001
-            #change minFreqLog in such a way smallest log(freq)-minFreqLog is greater than 0
-            self.minFreqLog = self.minFreqLog-((self.maxFreqLog-self.minFreqLog)/self.fontsizevariation)
-            
-            #change the font size between "-2" to "+8" relative to current font size
-            tagHtmlStr = ' '.join([('<font size="%+d" class="tagword">%s(%d)</font>\n'%(self.__getTagFontSize(freq), x,freq))
-                                       for x,freq in tagWordList])
-        return(tagHtmlStr)
-    
     def getTagCloudJSON(self, numWords=100, filterFunc=None):
         tagJsonStr = ''
 
