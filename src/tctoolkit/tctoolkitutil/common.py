daf9d9a7c5f53d5cca3362d0bc7b6454e7cefc75
nitinbhide <nitinbhide@localhost>
2014-03-24 13:32:15 +0530

    refactored file list finder functions into a DirFileLister class.

--- src/tctoolkit/tctoolkitutil/common.py
+++ src/tctoolkit/tctoolkitutil/common.py
@@ -9,14 +9,13 @@ New BSD License: http://www.opensource.org/licenses/bsd-license.php
 TC Toolkit is hosted at http://code.google.com/p/tctoolkit/
 '''
 
-import fnmatch
 import os
 import string
 import sys
 import time
+import re
 from contextlib import contextmanager
-
-IGNOREDIRS = set(['.svn','.cvs', '.hg', '.git'])
+    
 
 @contextmanager
 def FileOrStdout(filename):
@@ -48,72 +47,6 @@ def TimeIt(fout,prefix=''):
     timediff = end_time - start_time
     fout.write("%s : %.2f seconds" % (prefix, timediff))
     
-def RemoveIgnoreDirs(dirs):
-    '''
-    remove directories in the IGNOREDIRS list from the 'dirs'
-    '''
-    dirs2 = list(set(dirs) - IGNOREDIRS)    
-    dirs[:] = dirs2    
-
-def GetDirFileList(dirname):
-    rawfilelist = []
-    #prepare list of all files ignore the directories defined in 'ignoredirs' list.
-    for root, dirs, files in os.walk(dirname):
-        RemoveIgnoreDirs(dirs)
-        for fname in files:
-            rawfilelist.append(os.path.join(root, fname))
-    return(rawfilelist)
-
-def PreparePygmentsFileList(dirname):
-    '''
-    Use the lexer list and file extensions from the Pygments and prepare the list of files for which
-    lexers are available.
-    '''
-    import fnmatch,re
-    from pygments.lexers import get_all_lexers
-    
-    #Prepare a list of fnmatch patterns from lexers
-    fnmatchpatlist = []
-    for lexer in get_all_lexers():
-        fnmatchpatlist = fnmatchpatlist+[pat for pat in lexer[2]]
-
-    #since one fnmatch pattern can exist in multiple lexers. We need remove duplicates from the fnmatch pattern list
-    fnmatchpatlist=set(fnmatchpatlist)
-
-    #combine the match patterns to a single regex
-    matchregex = '|'.join([fnmatch.translate(pat) for pat in fnmatchpatlist])
-    
-    matchregex = re.compile(matchregex)
-    
-    rawfilelist = GetDirFileList(dirname)
-
-    filelist = []
-    for fname in rawfilelist:
-        if (matchregex.match(fname) != None):
-            filelist.append(fname)
-    return(filelist)
-
-def FindFileInPathList(fname, pathlist, extList=None):
-    '''
-    search the directories in 'pathlist' one by one to see if fname exists in that
-    directory. Search inside a directory is NOT recursive. First 'hit' is returned.
-    '''
-    patternList = []
-    if( extList != None and len(extList) > 0):
-        for exten in extList:
-            if( exten.startswith('.') == False):
-                exten = '.' + exten
-            patternList.append(fname + exten)
-    else:
-        patternList.append(fname)
-
-    
-    for fpath in pathlist:
-        for pattern in patternList:
-            testfname = os.path.join(fpath, pattern)        
-            if( os.path.exists(testfname)):
-                return(testfname)
-    return(None)
 
 def StripAtStart(src, strtostrip):
     if( src.startswith(strtostrip)):
