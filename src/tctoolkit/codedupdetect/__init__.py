6ae141e9eb242115d69e9ef0b3506045acb70d7f
Nitin Bhide <31431663+nitinbhide@users.noreply.github.com>
2014-09-06 11:29:41 +0530

    minor changes. added __all__ list.

--- src/tctoolkit/codedupdetect/__init__.py
+++ src/tctoolkit/codedupdetect/__init__.py
@@ -6,6 +6,9 @@ New BSD License: http://www.opensource.org/licenses/bsd-license.php
 TC Toolkit is hosted at https://bitbucket.org/nitinbhide/tctoolkit
 
 '''
+
+__all__ = ['tokenizer', 'codedupdetect', 'matchstore', 'rabinkarp']
+
 from .tokenizer import Tokenizer
 from .codedupdetect import CodeDupDetect
 from .matchstore import MatchData
