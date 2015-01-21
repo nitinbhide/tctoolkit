ac0fb00143974be19017d75a0ee666e0f823dad0
manojp <manoj.a.phatak@gmail.com>
2015-01-21 10:09:05 +0530

    Sript for extracting cdd data in a form which can be consumed by another program : derived class injected into main flow

--- src/tctoolkit/code_duplication_extractor.py
+++ src/tctoolkit/code_duplication_extractor.py
@@ -11,7 +11,18 @@ from tctoolkit.codedupdetect import CodeDupDetect
 from tctoolkit import cdd
 
 def extract_duplicates():
+    cdd.getApp = lambda parser : DuplicatesExtractor(parser)
     cdd.RunMain()
+
+
+class DuplicatesExtractor(cdd.CDDApp):
+     def _run(self):
+        filelist = self.getFileList(self.args[0])
+        self.cdd = CodeDupDetect(filelist,self.options.minimum, fuzzy=self.options.fuzzy,\
+                                 min_lines=self.options.min_lines, blameflag=self.options.blame)
+
+        self.printDuplicates(self.outfile)
+
 
 if __name__=='__main__':
     extract_duplicates()
