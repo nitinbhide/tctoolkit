360e89a278203a45319a7bedb68479c2e8512d5e
manojp <none@none>
2015-01-21 14:44:26 +0530

    Sript for extracting cdd data in a form which can be consumed by another program: minor refactoring, default options moved to constructor

--- src/tctoolkit/cdd_tests.py
+++ src/tctoolkit/cdd_tests.py
@@ -6,30 +6,31 @@ import unittest
 import code_duplication_extractor as dups_extractor
 import optparse
 
-class Options: pass
+class Options:
+    def __init__(self):
+        self.format = 'txt'
+        self.pattern = ""
+        self.log = False
+        self.report = None
+        self.minimum = 100
+        self.fuzzy = False
+        self.min_lines = 3
+        self.blame = False
+
 
 def inject_parse_args():
     options = Options()
-    args = []
-
     options.format = 'txt'
     options.lang = 'py'
     options.outfile = './testdata/outfile'
-    options.pattern = ""
-    options.log = False
-    options.report = None
-    options.minimum = 100
-    options.fuzzy = False
-    options.min_lines = 3
-    options.blame = False
 
-    args.append('./testdata')
-
+    args = ['./testdata']
     return options,args
+
 
 class TestFixture(unittest.TestCase):
     def setUp(self):
-        mock_parser = optparse.OptionParser("", description="")
+        mock_parser = optparse.OptionParser()
         mock_parser.parse_args = inject_parse_args
         dups_extractor.get_option_parser = lambda : mock_parser
 
@@ -38,10 +39,6 @@ class TestFixture(unittest.TestCase):
         expected = {1: {'linecount': 37, 'fcount': 2}, 2: {'linecount': 30, 'fcount': 2},
                     3: {'fcount': 2,'linecount': 20}, 4: {'linecount': 18, 'fcount': 3}}
         self.assertEqual(expected,dups)
-
-
-
-
 
 if __name__=='__main__':
     unittest.main()
