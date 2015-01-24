4d8a57390f0d00e4dd4f084983d6c113d07820b2
manojp <none@none>
2015-01-24 15:41:27 +0530

    minor refactoring to improve readability

--- src/tctoolkit/cdd_tests.py
+++ src/tctoolkit/cdd_tests.py
@@ -6,30 +6,33 @@ import os
 import cdd
 import code_duplication_extractor as dups_extractor
 
-class Options:
-    def __init__(self):
-        self.format = 'txt'
-        self.pattern = ""
-        self.log = False
-        self.report = None
-        self.minimum = 100
-        self.fuzzy = False
-        self.min_lines = 3
-        self.blame = False
-        self.outfile = '.\\testdata\out.txt'
-        self.runtests = False
-        self.comments = False
+def get_default_options():
+    class Options:
+        def __init__(self):
+            self.format = 'txt'
+            self.pattern = ""
+            self.log = False
+            self.report = None
+            self.minimum = 100
+            self.fuzzy = False
+            self.min_lines = 3
+            self.blame = False
+            self.outfile = '.\\testdata\out.txt'
+            self.runtests = False
+            self.comments = False
+    return Options()
+
 
 def get_option_parser(lang,srclocation):
         parser = cdd.createOptionParser()
-        def inject_parse_args():
-            options = Options()
+        def parse_args():
+            options = get_default_options()
             assert os.path.exists(srclocation), 'target src path is invalid'
             options.lang = lang
             args = [srclocation]
             return options,args
 
-        parser.parse_args = inject_parse_args
+        parser.parse_args = parse_args # inject our own version of parse_args() function, that will return what we teach him
         return parser
 
 class TestFixture(unittest.TestCase):
