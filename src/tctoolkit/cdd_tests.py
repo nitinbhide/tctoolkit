d5a4548527ca582e56f7d2ec34cf6f98d70c701a
manojp <manoj.a.phatak@gmail.com>
2015-02-01 22:40:59 +0530

    Backed out changeset: 29252de08147

--- src/tctoolkit/cdd_tests.py
+++ src/tctoolkit/cdd_tests.py
@@ -1,8 +1,4 @@
 '''
-This module is part of Thinking Craftsman Toolkit (TC Toolkit).
-and is released under the New BSD License: http://www.opensource.org/licenses/bsd-license.php
-TC Toolkit is hosted at https://bitbucket.org/nitinbhide/tctoolkit
-
 Purpose: Tests for cdd
 '''
 import unittest
