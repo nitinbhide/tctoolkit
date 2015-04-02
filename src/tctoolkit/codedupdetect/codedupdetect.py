be53cafbb9af6f6c4ac5719718811dd564d3c6e4
nitinbhide <nitinbhide@thinkingcraftsman.in>
2015-04-02 14:43:30 +0530

    Fixed a few pylint errors.

--- src/tctoolkit/codedupdetect/codedupdetect.py
+++ src/tctoolkit/codedupdetect/codedupdetect.py
@@ -14,14 +14,13 @@ import logging
 import tempfile
 import os
 import shutil
-from itertools import tee,izip
+from itertools import tee, izip
 
 import matchstore
 from rabinkarp import RabinKarp
-from tokenizer import Tokenizer
 
 class CodeDupDetect(object):
-    def __init__(self,filelist, minmatch=100, fuzzy=False,min_lines=3,blameflag=False):
+    def __init__(self, filelist, minmatch=100, fuzzy=False, min_lines=3, blameflag=False):
         self.matchstore = matchstore.MatchStore(minmatch, blameflag)
         self.minmatch = minmatch #minimum number of tokens to be matched.
         self.min_lines = min_lines # minimum number of lines to match
@@ -31,58 +30,58 @@ class CodeDupDetect(object):
 
     def __findcopies(self):
         totalfiles = len(self.filelist)
-        
-        rk = RabinKarp(self.minmatch,self.min_lines, self.matchstore, self.fuzzy)
+
+        rk = RabinKarp(self.minmatch, self.min_lines, self.matchstore, self.fuzzy)
             
         for i, srcfile in enumerate(self.filelist):
-            print "Analyzing file %s (%d of %d)" %(srcfile,i+1,totalfiles)
-            logging.info("Analyzing file %s (%d of %d)" %(srcfile,i+1,totalfiles))
+            print "Analyzing file %s (%d of %d)" %(srcfile, i+1, totalfiles)
+            logging.info("Analyzing file %s (%d of %d)" %(srcfile, i+1, totalfiles))
             rk.addAllTokens(srcfile)
         self.foundcopies = True
 
     def findcopies(self):
-        if( self.foundcopies == False):
+        if self.foundcopies == False:
             self.__findcopies()
-        return(self.matchstore.iter_matches())
+        return self.matchstore.iter_matches()
 
-    def printmatches(self,output):
+    def printmatches(self, output):
         exactmatches = self.findcopies()
         #now sort the matches based on the matched line count (in reverse)
-        exactmatches = sorted(exactmatches,reverse=True,key=lambda x:x.matchedlines)
-        matchcount=0
+        exactmatches = sorted(exactmatches, reverse=True, key=lambda x: x.matchedlines)
+        matchcount = 0
 
         for matches in exactmatches:
-                output.write('%s\n'%('='*50))
-                matchcount=matchcount+1
-                output.write("Match %d:\n"%matchcount)
-                fcount = len(matches)
-                first = True
-                for match in matches:
-                        if( first):
-                                output.write("Found an approx. %d line duplication in %d files.\n" % (match.getLineCount(),fcount))
-                                first = False
-                        output.write("Starting at line %d of %s\n" % (match.getStartLine(),match.srcfile()))
+            output.write('%s\n' % ('='*50))
+            matchcount=matchcount+1
+            output.write("Match %d:\n" % matchcount)
+            fcount = len(matches)
+            first = True
+            for match in matches:
+                if first:
+                    output.write("Found an approx. %d line duplication in %d files.\n" % (match.getLineCount(), fcount))
+                    first = False
+                output.write("Starting at line %d of %s\n" % (match.getStartLine(), match.srcfile()))
 
-        return(exactmatches)
+        return exactmatches
 
     def insert_comments(self, dirname):
         begin_no = 0
-        for matches in sorted(self.findcopies(),reverse=True,key=lambda x:x.matchedlines):
+        for matches in sorted(self.findcopies(), reverse=True, key=lambda x: x.matchedlines):
             for match in matches:
                 fn = match.srcfile()
-                infostring = ' '.join(['%s:%i+%i'%(f.srcfile(),f.getStartLine(),f.getLineCount()) for f in matches if f.srcfile() != fn]).replace(dirname, '')
+                infostring = ' '.join(['%s:%i+%i' % (f.srcfile(), f.getStartLine(), f.getLineCount()) for f in matches if f.srcfile() != fn]).replace(dirname, '')
                 tmp_source = tempfile.NamedTemporaryFile(mode='wb', delete=False, prefix='cdd-')
                 with open(fn, 'r') as srcfile:
                     for i in range(match.getStartLine()):
                         tmp_source.write(srcfile.readline())
-                    comment = '//!DUPLICATE BEGIN %i -- %s\n'%(begin_no, infostring)
+                    comment = '//!DUPLICATE BEGIN %i -- %s\n' % (begin_no, infostring)
                     tmp_source.write(comment)
                     for i in range(match.getLineCount()):
                         line = srcfile.readline()
                         tmp_source.write(line)
                         #with open('/tmp/%i.dup'%begin_no, 'a') as dup:
                         #    dup.write(line)
-                    comment = '//!DUPLICATE END %i\n'%begin_no
+                    comment = '//!DUPLICATE END %i\n'% begin_no
                     tmp_source.write(comment)
                     line = srcfile.readline()
                     while line:
@@ -105,9 +104,9 @@ class CodeDupDetect(object):
 
         #add group (directory of the file) into the groups list
         def addGroup(filename):
-            dir = os.path.dirname(filename)
-            if dir not in groups:
-                groups[dir] = len(groups)
+            dir_of_file = os.path.dirname(filename)
+            if dir_of_file not in groups:
+                groups[dir_of_file] = len(groups)
 
         #add file into file list
         def addNode(filename):
@@ -121,15 +120,15 @@ class CodeDupDetect(object):
             file1 = os.path.relpath(match1.srcfile(), dirname)
             file2 = os.path.relpath(match2.srcfile(), dirname)
             if file1 > file2:
-                file1,file2 = file2, file1
-            linkinfo = links.get((file1,file2), {'lines':0, 'count':0})
+                file1, file2 = file2, file1
+            linkinfo = links.get((file1, file2), {'lines':0, 'count':0})
             linkinfo['lines'] = linkinfo['lines']+match1.getLineCount()
             linkinfo['count'] = linkinfo['count']+1
-            links[(file1,file2)] = linkinfo
+            links[(file1, file2)] = linkinfo
         
         #copied from python recipes to get 'two' items at a time from list. (n, n+1)
         def pairwise(iterable):
-            "s -> (s0,s1), (s1,s2), (s2, s3), ..."
+            "s -> (s0, s1), (s1, s2), (s2, s3), ..."
             a, b = tee(iterable)
             next(b, None)
             return izip(a, b)
