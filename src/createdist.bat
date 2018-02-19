6562193746795939eb1bbfea20f03369bfbd4600
Nitin Bhide <31431663+nitinbhide@users.noreply.github.com>
2018-02-19 19:00:18 +0530

    python path changed.

--- src/createdist.bat
+++ src/createdist.bat
@@ -3,7 +3,7 @@ REM Creating binrary and source distribution of SVNPlot
 del /s /q .\build\*
 del manifest
 echo "Creating Thinking Craftsman Toolkit source distribution in zip format"
-python setup.py sdist --formats=zip
+py -2 setup.py sdist --formats=zip
 echo "Creating Windows installer for Thinking Craftsman Toolkit "
-python setup.py bdist_wininst
+py -2 setup.py bdist_wininst
 
